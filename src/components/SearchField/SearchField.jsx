535c7bef9790cc8c64d9a0e151a9cd8bab0fb67b
julianbooher <julianbooher@hotmail.com>
2021-07-23 10:55:24 -0500

    made SearchField it's own component

---
+++ src/components/SearchField/SearchField.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+import { Form, Button } from 'react-bootstrap';
+
+
+function SearchField(props) {
+
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const search = (event) => {
+    event.preventDefault();
+    console.log('inside search, searchQuery: ', searchQuery);
+  }
+
+  return (
+    <div>
+      <Form
+        className="card-search-form"
+        onSubmit={e => { search(e) }}
+      >
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Card</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Search for a card name."
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+          />
+        </Form.Group>
+        <Button
+          className="btn card-search-btn"
+          type="submit"
+          name="submit"
+        >
+          Search
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default connect(mapStoreToProps)(SearchField);
--- src/components/UserHome/UserHome.jsx
+++ src/components/UserHome/UserHome.jsx
@@ -1,42 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import { Form, Button } from 'react-bootstrap';
+import SearchField from '../SearchField/SearchField.jsx'
 
 
 function UserHome(props) {
 
-  const [searchQuery, setSearchQuery] = useState('')
-
-  const search = (event) => {
-    event.preventDefault();
-    console.log('inside search, searchQuery: ', searchQuery);
-  }
-
   return (
     <div>
       <h2>User Home</h2>
-      <Form
-        className="card-search-form"
-        onSubmit={e => { search(e) }}
-      >
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Card</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Search for a card name."
-            value={searchQuery}
-            onChange={e => setSearchQuery(e.target.value)}
-          />
-        </Form.Group>
-        <Button
-          className="btn card-search-btn"
-          type="submit"
-          name="submit"
-        >
-          Search
-        </Button>
-      </Form>
+      <SearchField />
     </div>
   );
 }
