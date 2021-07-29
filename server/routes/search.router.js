0a49d6c1a8aa812d57d44015edeadb32eabba564
julianbooher <julianbooher@hotmail.com>
2021-07-29 18:53:04 -0500

    search route working, gets 10 results at a time

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -6,20 +6,21 @@ require('dotenv').config();
 
 
 // Get list of search results from API call.
-router.get('/', async (req, res) => {
+router.get('/:searchPage/:searchValue', (req, res) => {
 
     const version = process.env.version;
     const accessToken = process.env.accessToken;
 
-    const searchData = 
-    {
+    console.log('inside search router, params', req.params)
+
+    const searchData = {
         "sort": "name",
         "limit": 10,
         "offset": req.params.searchPage - 1,
         "filters": [
             { "name": "ProductName", "values": [ req.params.searchValue ] }
         ]    
-    }
+    };
     
     axios.post(`https://api.tcgplayer.com/${version}/catalog/categories/1/search`, searchData, {
         "headers": {
@@ -27,8 +28,13 @@ router.get('/', async (req, res) => {
             "Content-Type": 'application/json'
         }
     })
-
-    console.log('inside search router');    
+    .then (response => {
+        res.send(response.data);
+    })
+    .catch( error => {
+        console.log('Error inside POST request in search', error);
+        res.sendStatus(500);
+    })
     
 });
 
