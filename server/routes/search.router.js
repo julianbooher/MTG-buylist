cee760c243ff21a092d8c8ad0d5e8017db7bd617
julianbooher <julianbooher@hotmail.com>
2021-07-29 18:28:15 -0500

    initialized search router for testing on postman

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -8,6 +8,26 @@ require('dotenv').config();
 // Get list of search results from API call.
 router.get('/', async (req, res) => {
 
+    const version = process.env.version;
+    const accessToken = process.env.accessToken;
+
+    const searchData = 
+    {
+        "sort": "name",
+        "limit": 10,
+        "offset": req.params.searchPage - 1,
+        "filters": [
+            { "name": "ProductName", "values": [ req.params.searchValue ] }
+        ]    
+    }
+    
+    axios.post(`https://api.tcgplayer.com/${version}/catalog/categories/1/search`, searchData, {
+        "headers": {
+            "Authorization": `Bearer ${accessToken}`,
+            "Content-Type": 'application/json'
+        }
+    })
+
     console.log('inside search router');    
     
 });
